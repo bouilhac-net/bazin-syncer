975d3986754490c5ae469b4a130ca16d9714863b
Yann Bouilhac <yann@bouilhac.net>
2025-07-16 11:41:07 +0200

    Update syncer to 1.8.2 Update France Casse module to 1.1.0

--- src/FranceCasse/Manager/PartManager.php
+++ src/FranceCasse/Manager/PartManager.php
@@ -9,6 +9,7 @@ class PartManager extends \SolutionsCVHU\Syncer\FranceCasse\Manager\PartManager
         return 'opisto_part.blocked = 0 AND
                 opisto_part.available = 1 AND
                 opisto_part.for_sale = 1 AND
-                opisto_part.in_stock = 1';
+                opisto_part.in_stock = 1 AND
+                (s.id = 1643 OR s.id = 1722)';
     }
 }
